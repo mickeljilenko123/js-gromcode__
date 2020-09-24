a185b627362008da36a04c61daca4542df4fb811
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-24 22:13:13 +0300

    lesson2 commit

--- lesson_2/task_3/index.js
+++ lesson_2/task_3/index.js
@@ -6,5 +6,5 @@ console.log(anotherNumber);
 let customer = { name: 'John', age: 17 };
 anotherCustomer = customer;
 anotherCustomer.age = 20;
-console.log(customer);
-console.log(anotherCustomer);
+console.log(customer.age);
+console.log(anotherCustomer.age);
