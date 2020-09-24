478940bd9524155d7f5506fb0a54676f6d958abc
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-24 22:06:06 +0300

    lesson2 commit

---
+++ lesson_2/task_3/index.js
@@ -0,0 +1,10 @@
+let goodNumber = 17;
+let anotherNumber = goodNumber;
+anotherNumber = 77;
+console.log(goodNumber);
+console.log(anotherNumber);
+let customer = { name: 'John', age: 17 };
+anotherCustomer = customer;
+anotherCustomer.age = 20;
+console.log(customer);
+console.log(anotherCustomer);
