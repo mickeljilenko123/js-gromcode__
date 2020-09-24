1f9eeae2894ec4219ee9167b9c58bc742dd68ffb
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-24 21:35:13 +0300

    lesson 2 commit

---
+++ lesson 2/task2/index.js
@@ -0,0 +1,8 @@
+const userName = 'John';
+const greeting = "I'm John";
+const profession = 'Developer';
+const hobby = 'Walking';
+const favouriteDESSERT = 'cake';
+let pet = 'dog';
+let superNumber;
+superNumber = 17;
