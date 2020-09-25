b9f1559906316a0870515dce1d1069972e265b26
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-26 00:24:46 +0300

    js5 commit

--- lesson_5/hw_2/index.js
+++ lesson_5/hw_2/index.js
@@ -1,8 +1,9 @@
 const increaser = (a, index) => {
-    while (a < index) {
-        a += index;
+    if (a <= index) {
+        return a += index;
+    } else {
+        return a;
     }
-    return a;
 }
 const res = increaser(2, 10);
 console.log(res);
