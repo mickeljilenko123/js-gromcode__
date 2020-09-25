b6abd010026a3e2ea2b8ca5fee3e83c3da7ae7c5
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-25 15:47:32 +0300

    js4 commit

---
+++ lesson_4/hw_2/index.js
@@ -0,0 +1,17 @@
+let m = 2;
+const n = 45;
+let result = 0;
+
+while (m <= n) {
+    if (m % 2 === 0 || m % 4 !== 0) {
+        result += m;
+    }
+    if (m % 3 === 0) {
+        result -= m;
+    }
+    if (m % 5 === 0) {
+        console.log(m);
+    }
+    m++;
+}
+console.log(result);
