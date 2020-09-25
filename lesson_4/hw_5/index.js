7a3a1594c82e3ad4a62e7f9e594d6bf921adfbc6
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-25 19:18:10 +0300

    js3 commit

--- lesson_4/hw_5/index.js
+++ lesson_4/hw_5/index.js
@@ -2,6 +2,10 @@ let sum = 0;
 for (let i = 1; i <= 1000; i++) {
     sum += i;
 }
+let c = sum / 1234;
+// console.log(c); //406
+let d = sum % 1234;
+// console.log(d); //730
 
-let bool = (sum / 1234 > sum % 1234) || (sum / 1234 < sum % 1234);
+let bool = (c > d) || (c < d);
 console.log(bool);
