f93a2b947d77bcad7ca3e90121d06e9c63745a39
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-25 19:52:02 +0300

    js5 commit

--- lesson_4/hw_2/index.js
+++ lesson_4/hw_2/index.js
@@ -6,12 +6,15 @@ let result = 0;
 while (a <= n) {
     if (a % 2 === 0 && a % 4 !== 0) {
         result += a;
+        continue;
     }
     if (a % 3 === 0) {
         result -= a;
+        continue;
     }
     if (a % 4 === 0) {
         result *= a;
+        continue;
     }
     if (a % 5 === 0) {
         console.log(a);
