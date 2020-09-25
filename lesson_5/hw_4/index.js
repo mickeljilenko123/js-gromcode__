aabe17d8f9cfb4db40b0a20fe54cc607b3d1a5d9
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-26 01:03:58 +0300

    js5 commit

---
+++ lesson_5/hw_4/index.js
@@ -0,0 +1,12 @@
+const findDivCount = (a, b, n) => {
+    let i = 0;
+    while (a < b) {
+        if (a % n === 0) {
+            i++;
+        }
+        a++;
+    }
+    return i;
+}
+const result = findDivCount(2, 8, 3);
+console.log(result);
