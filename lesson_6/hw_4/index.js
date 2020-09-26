be638a62419f1c7f7e90d279baad2864705b9732
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-27 00:01:48 +0300

    js6 commit

--- lesson_6/hw_4/index.js
+++ lesson_6/hw_4/index.js
@@ -2,4 +2,16 @@ const checkSum = (arr) => {
     if (!Array.isArray(arr)) {
         return null;
     }
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    if (sum > 100) {
+        return true;
+    } else {
+        return false;
+    }
 }
+const num = [44, 72, 8, 2];
+const res = checkSum(num);
+console.log(res);
