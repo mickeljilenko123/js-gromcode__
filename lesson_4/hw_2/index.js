1faf008644522809520bab637d884025a50df12d
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-26 13:32:01 +0300

    js5 commit

--- lesson_4/hw_2/index.js
+++ lesson_4/hw_2/index.js
@@ -1,5 +1,5 @@
 const m = 2;
-const n = 16;
+const n = 45;
 let a = m;
 let result = 0;
 
@@ -9,20 +9,15 @@ while (a <= n) {
     }
     if (a % 3 === 0) {
         result *= a;
-        // console.log('a % 3 === 0 ' + result);
     }
-
+    if (a % 5 === 0) {
+        console.log(a);
+    }
     if (a % 2 === 0 && a % 4 !== 0) {
         result += a;
-        // console.log('a % 2 === 0 && a % 4 !== 0 ' + result);
     }
     if (a % 4 === 0) {
         result -= a;
-        // console.log('a % 4 === 0 ' + result);
-    }
-    if (a % 5 === 0) {
-        console.log('a % 5 === 0' + a);
-        // console.log('a % 3 === 0 ' + result);
     }
     a++;
 }
