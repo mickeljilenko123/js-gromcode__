5cb5ecfc8458fc43e8f92879cfd1b0f44e825ac8
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-26 16:03:03 +0300

    js5 commit

--- lesson_6/task_2/index.js
+++ lesson_6/task_2/index.js
@@ -1,5 +1,8 @@
 const num = [2, 4, 6, 7]
 const getSum = (arr) => {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
