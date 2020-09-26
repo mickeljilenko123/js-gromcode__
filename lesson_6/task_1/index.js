fb0850d5eeec2c1935ebdfe1c72ba408aa279237
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-26 15:54:47 +0300

    js5 commit

--- lesson_6/task_1/index.js
+++ lesson_6/task_1/index.js
@@ -2,7 +2,7 @@ const getArrayBounds = (arr) => {
     if (!Array.isArray(arr)) {
         return null;
     }
-    return (arr.length, arr[0], arr.length - 1);
+    return [arr.length, arr[0], arr[arr.length - 1]];
 }
 const arrayBounds = getArrayBounds([12, 66, 23, 13]);
 console.log(arrayBounds);
