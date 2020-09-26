ced9130f0230e4962b7f22566008963c36aae21c
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-26 13:10:03 +0300

    js5 commit

--- lesson_4/hw_2/index.js
+++ lesson_4/hw_2/index.js
@@ -1,24 +1,24 @@
-const m = 15;
-const n = 45;
+const m = 2;
+const n = 16;
 let a = m;
 let result = 0;
 
 while (a <= n) {
     if (a % 5 === 0) {
-        console.log(a);
-        break;
-    }
-    if (a % 4 === 0) {
-        result *= a;
-        break;
-    }
-    if (a % 3 === 0) {
-        result -= a;
-        break;
+        console.log('a % 5 === 0 ' + a);
     }
     if (a % 2 === 0 && a % 4 !== 0) {
         result += a;
-        break;
+        console.log('a % 2 === 0 && a % 4 !== 0 ' + result);
+    }
+    if (a % 3 === 0) {
+        result *= a;
+        console.log('a % 3 === 0 ' + result);
+    }
+    if (a % 4 === 0) {
+        result -= a;
+        console.log('a % 4 === 0 ' + result);
     }
     a++;
 }
+console.log(result);
