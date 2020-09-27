93431ca5eb575607b6319f5c299b67e7c9dba38a
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-28 00:41:19 +0300

    js6 commit

---
+++ lesson_6/hw_10/index.js
@@ -0,0 +1,18 @@
+const clients = ['Ann', 'John', 'User'];
+const client = 'User';
+const balances = [1400, 87, -6];
+const amount = 10;
+
+const withdraw = (clients, client, balances) => {
+    for (let i = 0; i < balances.length; i++) {
+        if (balances.indexOf(balances[i]) === clients.indexOf(client)) {
+            if (balances[i] >= amount) {
+                balances[i] -= amount;
+            } else {
+                return -1;
+            }
+        }
+    }
+    return balances;
+}
+console.log(withdraw(clients, client, balances));
