fdec955bc63d7feb5a8e8694ee3a3ddd759f1825
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-27 20:06:31 +0300

    js6 commit

--- lesson_6/hw_9/index.js
+++ lesson_6/hw_9/index.js
@@ -1,36 +1,36 @@
 const num = [2, 15, 3, 10, 8];
-const [...num1] = num;
+// const [...num1] = num;
 const sortAsc = (arr) => {
     if (!Array.isArray(arr)) {
         return null;
     }
-    for (let n = 0; n < num1.length; n++) {
-        for (let i = 0; i < num1.length - 1; i++) {
-            if (num1[i] > num1[i + 1]) {
-                let sort = num1[i];
-                num1[i] = num1[i + 1];
-                num1[i + 1] = sort;
+    for (let n = 0; n < num.length; n++) {
+        for (let i = 0; i < num.length - 1; i++) {
+            if (num[i] > num[i + 1]) {
+                let sort = num[i];
+                num[i] = num[i + 1];
+                num[i + 1] = sort;
             }
         }
     }
-    return num1;
+    return num;
 }
-console.log(sortAsc(num1));
+console.log(sortAsc(num));
 
 
 const sortDesc = (arr) => {
     if (!Array.isArray(arr)) {
         return null;
     }
-    for (let n = 0; n < num1.length; n++) {
-        for (let i = 0; i < num1.length - 1; i++) {
-            if (num1[i] < num1[i + 1]) {
-                let sort = num1[i];
-                num1[i] = num1[i + 1];
-                num1[i + 1] = sort;
+    for (let n = 0; n < num.length; n++) {
+        for (let i = 0; i < num.length - 1; i++) {
+            if (num[i] < num[i + 1]) {
+                let sort = num[i];
+                num[i] = num[i + 1];
+                num[i + 1] = sort;
             }
         }
     }
-    return num1;
+    return num;
 }
-console.log(sortDesc(num1));
+console.log(sortDesc(num));
