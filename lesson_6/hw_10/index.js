801bc34db68685262a2b6accf521171ee94b4c75
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-28 12:29:02 +0300

    js6 commit

--- lesson_6/hw_10/index.js
+++ lesson_6/hw_10/index.js
@@ -1,18 +1,39 @@
+// const clients = ['Ann', 'John', 'User'];
+// const client = 'John';
+// const balances = [1400, 87, -6];
+// const amount = 50;
+
+// const withdraw = (clients, client, balances, amount) => {
+//     for (let i = 0; i < balances.length; i++) {
+//         if (balances.indexOf(balances[i]) === clients.indexOf(client)) {
+//             if (balances[i] > amount) {
+//                 balances[i] -= amount;
+//             } else if (balances[i] < amount) {
+//                 return -1;
+//             }
+//         }
+//     }
+//     return balances;
+// }
+// console.log(withdraw(clients, client, balances, amount));
+
+// else variant
+
 const clients = ['Ann', 'John', 'User'];
 const client = 'John';
-const balances = [1400, 87, -6];
+const balances = [1400, 94, -6];
 const amount = 50;
 
-const withdraw = (clients, client, balances, amount) => {
-    for (let i = 0; i < balances.length; i++) {
-        if (balances.indexOf(balances[i]) === clients.indexOf(client)) {
+function withdraw(clients, balances, client, amount) {
+    for (let i = 0; i < clients.length; i++) {
+        if (clients[i] == client) {
             if (balances[i] > amount) {
-                balances[i] -= amount;
-            } else if (balances[i] < amount) {
+                return balances[i] -= amount;
+            }
+            if (balances[i] < amount) {
                 return -1;
             }
         }
     }
-    return balances;
 }
-console.log(withdraw(clients, client, balances, amount));
+console.log(withdraw(clients, balances, client, amount));
