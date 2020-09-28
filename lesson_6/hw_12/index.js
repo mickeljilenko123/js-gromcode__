6fc9b283383f9e5d33ae0ed472d42ea76d2bdd86
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-28 18:02:21 +0300

    js6 commit

--- lesson_6/hw_12/index.js
+++ lesson_6/hw_12/index.js
@@ -1,11 +1,15 @@
+// input arr, num
+// output 
+//1. Проходимся циклом по массиву 
+//2. Пушим каждый эл массива в новый массив пока i < num;
 const arr = [11, 4, 8, 3];
 let num = 3;
-
 const getSubArray = (arr, num) => {
     let arr1 = [];
+    let num1 = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (arr.includes(arr[i]) === num) {
-            return
+        if (num1 === num) {
+            console.log(arr[i]);
         }
     }
 }
