c229abde95de3d42ff20974abf779b69cdf0bfda
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-29 23:50:41 +0300

    js6 commit

--- lesson_7/task_1/index.js
+++ lesson_7/task_1/index.js
@@ -1,6 +1,6 @@
-const numbers = [2, 4, 3, 6];
+const arr = [2, 4, 3, 6];
 
-const getSpecialNumbers = () => {
-    return numbers.filter((num) => num % 3 === 0);
-}
-console.log(getSpecialNumbers(numbers));
+const getSpecialNumbers = (numbers) =>
+    numbers.filter((num) => num % 3 === 0);
+
+console.log(getSpecialNumbers(arr));
