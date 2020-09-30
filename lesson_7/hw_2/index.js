bbbe6636d3e6a85058e306abe9accf67f2a366f7
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-30 17:07:58 +0300

    js7 commit

--- lesson_7/hw_2/index.js
+++ lesson_7/hw_2/index.js
@@ -1,11 +1,11 @@
 const numbers = [2, 5, 8, 3, 9];
-
+const num = [...numbers];
 
 const reverseArray = (arr) => {
     if (!Array.isArray(arr)) {
         return null;
     }
-    const num = [...numbers];
-    return num.reverse();
+
+    return arr.reverse();
 }
-console.log(reverseArray(numbers));
+console.log(reverseArray(num));
