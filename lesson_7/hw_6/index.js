14f8488cfa9e4b0e442d5e5f7adb76d80842271c
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-30 19:23:52 +0300

    js7 commit

--- lesson_7/hw_6/index.js
+++ lesson_7/hw_6/index.js
@@ -1,9 +1,9 @@
 const numbers = [2, 4, 6, 8, 9, 19];
 
-const getSum = (arr) => {
+const sum = (arr) => {
     if (!Array.isArray(arr)) {
         return null;
     }
     return arr.reduce((acc, num) => acc + num, 0);
 }
-console.log(getSum(numbers));
+console.log(sum(numbers));
