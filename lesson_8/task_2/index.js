6d90b280e90b5d925f185d7e0b6f11123b6e7f8b
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-30 23:56:23 +0300

    js8 commit

---
+++ lesson_8/task_2/index.js
@@ -0,0 +1,11 @@
+const obj1 = { name: 'John Doe', age: 17, interest: 'football' }
+
+const concatProps = (obj) => {
+    let arr = [];
+    for (const key in obj) {
+        // arr.push(obj[key]);
+        arr = arr.concat(obj[key])
+    }
+    return arr;
+}
+console.log(concatProps(obj1));
