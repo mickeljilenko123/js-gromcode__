63f64bffb02b95d3afef156e2f621d50bbd08e23
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-30 13:33:33 +0300

    js7 commit

---
+++ lesson_7/task_2/index.js
@@ -0,0 +1,11 @@
+// input [1,6,9,3];
+// output [1,3,6,9];
+
+const arr = [1, 16, 3, 9];
+
+const sortDesc = (numbers) => {
+    const copy = [...numbers];
+    return copy.sort((a, b) => a - b);
+
+};
+console.log(sortDesc(arr));
