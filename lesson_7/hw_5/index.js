775e76e098c30930d954d06061b95c3914b8d7ad
mickeljilenko123 <mickeljilenko@gmail.com>
2020-09-30 19:22:03 +0300

    js7 commit

--- lesson_7/hw_5/index.js
+++ lesson_7/hw_5/index.js
@@ -1,17 +1,16 @@
-'use strict';
-
 const clients = ['Ann', 'John', 'User'];
 const balances = [1400, 87, -6];
 const client = 'John';
 const amount = 50;
 
-const withdraw = (clients, balances, client, amount) => {
+const withdraw = (clients1, balances1, client1, amount1) => {
     let current = 0;
-    balances.forEach((elem) => {
-        if (balances.indexOf(elem) === clients.indexOf(client)) {
+    balances1.forEach((elem) => {
+        if (balances1.indexOf(elem) === clients1.indexOf(client1)) {
             current += elem;
         }
     });
-    return (current > amount) ? current -= amount : -1;
+    // eslint-disable-next-line no-return-assign
+    return current > amount1 ? current -= amount1 : -1;
 }
 console.log(withdraw(clients, balances, client, amount));
