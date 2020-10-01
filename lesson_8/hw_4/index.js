f25d00fa5a086b94e03dc887882d40a51831f218
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-01 13:05:57 +0300

    js8 commit

---
+++ lesson_8/hw_4/index.js
@@ -0,0 +1,28 @@
+const obj1 = { a: 17 }
+const obj2 = { a: 20, b: 55 }
+
+const mergeObjectsV1 = (obj1, obj2) => {
+    const obj3 = Object.assign({}, obj1, obj2);
+    return obj3;
+}
+console.log(mergeObjectsV1(obj1, obj2));
+
+const mergeObjectsV2 = (obj2, obj1) => {
+    const obj3 = Object.assign({}, obj2, obj1);
+    return obj3;
+}
+console.log(mergeObjectsV1(obj2, obj1));
+
+const mergeObjectsV3 = (obj1, obj2) => {
+    let obj3 = {...obj1 };
+    obj3 = {...obj2 };
+    return obj3
+}
+console.log(mergeObjectsV1(obj1, obj2));
+
+const mergeObjectsV4 = (obj2, obj1) => {
+    let obj3 = {...obj2 };
+    obj3 = {...obj1 };
+    return obj3
+}
+console.log(mergeObjectsV1(obj2, obj1));
