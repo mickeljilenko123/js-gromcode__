0e2f682b5d5426c6c2f9ca03ae9a2b703617c921
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-02 14:46:33 +0300

    js8 commit

---
+++ lesson_8/hw_7/index.js
@@ -0,0 +1,16 @@
+const obj1 = {
+    'John Doe': 19,
+    'Tom': 17,
+    'Bob': 18,
+}
+
+const getAdults = (obj) => {
+    const obj2 = {};
+    Object.keys(obj1).forEach((e) => {
+        if (obj1[e] >= 18) {
+            obj2[e] = obj1[e];
+        }
+    })
+    return obj2;
+}
+console.log(getAdults(obj1));
