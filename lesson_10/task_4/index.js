d2fe905fa49da11125a374961638fe9d675b5e88
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-06 18:43:22 +0300

    js10 commit

--- lesson_10/task_4/index.js
+++ lesson_10/task_4/index.js
@@ -1,7 +1,7 @@
 const numbers = [2.3, 2.5, -50, -99];
 
 const getMaxAbsoluteNumber = arr => {
-    if (!Array.isArray(arr) && (arr.length === 0)) {
+    if (!Array.isArray(arr) && (!arr.length === 0)) {
         return null;
     }
     const absoluteNum = arr.map(e => Math.abs(e));
