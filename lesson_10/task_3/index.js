97782a5c57d5480fd15ff1c399cbe967f843b7ed
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-06 18:53:11 +0300

    js10 commit

--- lesson_10/task_3/index.js
+++ lesson_10/task_3/index.js
@@ -1,8 +1,8 @@
-const numbers = 2.5;
+const numbers = -2.5;
 const multiRound = num => [
     Math.floor(num),
     Math.round(num),
     Math.ceil(num),
     Math.trunc(num), +num.toFixed(1),
 ];
-console.log(multiRound(numbers));
+console.log(multiRound(Math.abs(numbers)));
