d210130255565920ef77e5909dcac5b0ac296190
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-06 15:11:48 +0300

    js10 commit

---
+++ lesson_10/task_1/index.js
@@ -0,0 +1,31 @@
+// round - 17,4 (17) если 17,5 (18);
+// floor - 17,9 (17)
+// ceil - 17,1 (18)
+// trunc - 17,7 (17)
+const numbers = ['2,12jd', '16f', '24g', 2, 2.3, 15.8]
+const getFiniteNumbers = (num) =>
+    num.filter(e => Number.isFinite(e))
+console.log(getFiniteNumbers(numbers));
+
+
+
+const numbers = ['2,12jd', '16f', '24g', 2, 2.3, 15.8, NaN, undefined, NaN]
+const getFiniteNumbersV2 = num =>
+    num.filter(e => isFinite(e))
+console.log(getFiniteNumbersV2(numbers));
+
+
+const numbers = ['2,12jd', '16f', '24g', 2, 2.3, 15.8, NaN, undefined, NaN]
+const getNaN = num =>
+    num.filter(e => Number.isNaN(e))
+console.log(getNaN(numbers));
+
+const numbers = ['2,12jd', '16f', '24g', 2, 2.3, 15.8, NaN, undefined, NaN]
+const getNaNV2 = num =>
+    num.filter(e => isNaN(e))
+console.log(getNaNV2(numbers));
+
+const numbers = ['2,12jd', '16f', '24g', 2, 2.3, 15.8, NaN, undefined, NaN]
+const getIntegers = num =>
+    num.filter(e => Number.isInteger(e))
+console.log(getIntegers(numbers));
