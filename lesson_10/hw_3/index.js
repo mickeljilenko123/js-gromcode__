ac8072c585b45c9a6f5df9999607e0054efa0b49
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-07 12:34:48 +0300

    js10 commit

--- lesson_10/hw_3/index.js
+++ lesson_10/hw_3/index.js
@@ -1,12 +1,13 @@
-const num1 = 5.149;
+const num1 = -5.149;
 const num2 = 2;
 
 const superRound = (num1, num2) => {
+    const abs = Math.abs(num1);
     const arr = [
-        Math.floor(num1),
-        Math.round(num1),
-        Math.ceil(num1),
-        Math.trunc(num1), +num1.toFixed(num2),
+        Math.floor(abs),
+        Math.round(abs),
+        Math.ceil(abs),
+        Math.trunc(abs), +abs.toFixed(num2),
     ];
     return arr;
 }
