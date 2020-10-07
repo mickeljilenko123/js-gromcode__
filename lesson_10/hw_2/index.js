4cd021e6e1eac76ce9f019345f5da44f9af7e3f7
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-07 15:16:10 +0300

    js10 commit

--- lesson_10/hw_2/index.js
+++ lesson_10/hw_2/index.js
@@ -1,8 +1,20 @@
-const numbers = [1, 5];
+// input: length future array == n, start, end
+// output: array.length === n
 
-const getRandomNumbers = (arr, start, end) => {
-    let rand = start + Math.random() * (end - start);
-    return Math.floor(rand);
+const start = 1;
+const end = 10;
+const length = 4;
+
+const getRandomNumbers = (start, end, length) => {
+    const arr = [];
+
+    for (let i = 0; i < length; i++) {
+        let rand = start - 0.5 + Math.random() * (end - start + 1);
+        rand = Math.round(rand);
+        arr.push(rand);
+    }
+    return arr;
+
 }
 
-console.log(getRandomNumbers(numbers));
+console.log(getRandomNumbers(start, end, length));
