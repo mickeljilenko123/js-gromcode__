349d266689d6e29c55dbad72b5f8693f4d01c54f
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-07 20:36:49 +0300

    js11 commit

---
+++ lesson_11/task_2/index.js
@@ -0,0 +1,18 @@
+const arrContacts = [
+    { name: 'Tom', phoneNumber: '777-77-77' },
+    { name: 'Ann', phoneNumber: '777-77-77' },
+    { name: 'Bob', phoneNumber: '777-77-77' },
+    { name: 'Kriss', phoneNumber: '777-77-77' },
+];
+
+
+const sortContacts = (arr, isAsc) => {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    const result = arrContacts.sort((a, b) => {
+        return a.name.localeCompare(b.name)
+    });
+    return result;
+}
+console.log(sortContacts(arrContacts));
