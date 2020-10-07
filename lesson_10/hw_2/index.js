36a4fe12eec7dd33b209ed94ad89e6d24a590829
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-07 20:12:25 +0300

    js11 commit

--- lesson_10/hw_2/index.js
+++ lesson_10/hw_2/index.js
@@ -2,19 +2,16 @@
 // output: array.length === n
 
 const start = 1;
-const end = 10;
+const end = -10;
 const length = 4;
 
 const getRandomNumbers = (start, end, length) => {
     const arr = [];
-
     for (let i = 0; i < length; i++) {
-        let rand = start - 0.5 + Math.random() * (end - start + 1);
+        let rand = start + Math.random() * (end - start);
         rand = Math.round(rand);
         arr.push(rand);
     }
     return arr;
-
 }
-
 console.log(getRandomNumbers(start, end, length));
---
+++ lesson_11/task_1/index.js
@@ -0,0 +1,20 @@
+const text = 'mynameisvasya';
+const num = 4;
+
+const splitText = (text, num) => {
+    if (!text === String) {
+        return null;
+    }
+    let arr = [];
+    let subStr1 = 0;
+    while (true) {
+        let srtText = text.substr(subStr1, num);
+        if (srtText.length === 0) {
+            break;
+        }
+        arr.push(srtText[0].toUpperCase() + srtText.slice(1));
+        subStr1 += num;
+    }
+    return arr.join('\n');
+};
+console.log(splitText(text, num));
