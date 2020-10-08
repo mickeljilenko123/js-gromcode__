8114793eb0633d56702468b21fb44a3d84100b15
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-08 22:11:33 +0300

    js12 commit

--- lesson_12/task_1/index.js
+++ lesson_12/task_1/index.js
@@ -1,11 +1,10 @@
-const str = '1 * 2';
-
+const str = '1 + 2';
 
 const calc = str => {
     const a = str.split(' ');
     switch (a[1]) {
         case '+':
-            return +a[0] + +a[2];
+            return Number(a[0]) + Number(a[2]);
         case '-':
             return a[0] - a[2];
         case '*':
