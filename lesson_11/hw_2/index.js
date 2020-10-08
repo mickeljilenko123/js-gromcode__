cc01ed2d451e36c25ed5b3b394421f1d89de188b
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-08 17:08:59 +0300

    js11 commit

--- lesson_11/hw_2/index.js
+++ lesson_11/hw_2/index.js
@@ -2,7 +2,7 @@ const str = '123.2323.2321.453';
 const n = '.';
 
 const countOccurrences = (str, n) => {
-    if (typeof n !== "string") return null;
+    if (n !== '') return null;
     const count = (str.split(n).length - 1);
     return count;
 }
