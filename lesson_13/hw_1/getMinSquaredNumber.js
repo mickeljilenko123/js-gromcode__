d32fe626995b91b9cdbefa63846849fa164367e1
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-12 17:06:45 +0300

    js13 commit

--- lesson_13/hw_1/getMinSquaredNumber.js
+++ lesson_13/hw_1/getMinSquaredNumber.js
@@ -1,4 +1,7 @@
 export const getMinSquaredNumber = arr => {
+    if (!Array.isArray(arr) || (arr.length == 0)) {
+        return null;
+    }
     const arr1 = arr.map(e => Math.abs(e));
     const min = Math.min(...arr1);
     return min * min;
--- lesson_13/hw_1/getMinSquaredNumber.test.js
+++ lesson_13/hw_1/getMinSquaredNumber.test.js
@@ -1,5 +1,15 @@
 import { getMinSquaredNumber } from './getMinSquaredNumber.js';
 
+it('should get not array', () => {
+    const result = getMinSquaredNumber('notArray');
+    expect(result).toEqual(null);
+})
+
+it('should get array', () => {
+    const result = getMinSquaredNumber([]);
+    expect(result).toEqual(null);
+})
+
 it('should get min square number of array', () => {
     const result = getMinSquaredNumber([-777, 3, -2, 6, 45, -20]);
     expect(result).toEqual(4);
