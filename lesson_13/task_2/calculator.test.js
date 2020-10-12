7706a925fcec87b1c5fb4a5442b0c65882dc6fe8
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-12 16:26:05 +0300

    js13 commit

--- lesson_13/task_2/calculator.test.js
+++ lesson_13/task_2/calculator.test.js
@@ -1,4 +1,4 @@
-import { getSquaredArray, getOddNumbers } from './calculator.js';
+import getSum, { getSquaredArray, getOddNumbers } from './calculator.js';
 
 it('should get squared numbers', () => {
     const result = getSquaredArray([2, 4]);
@@ -8,4 +8,9 @@ it('should get squared numbers', () => {
 it('should get odd numbers', () => {
     const result = getOddNumbers([1, 2, 3, 4]);
     expect(result).toEqual([1, 3]);
+});
+
+it('should get sum of numbers', () => {
+    const result = getSum(8, 4);
+    expect(result).toEqual(12);
 });
