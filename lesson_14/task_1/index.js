0f1eaa58ae0173c1093d3d7f1581b2e4d800af28
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-13 16:39:44 +0300

    js14 commit

--- lesson_14/task_1/index.js
+++ lesson_14/task_1/index.js
@@ -1,13 +1,14 @@
+'use strict';
 let message = 'Just learn it!';
-export const sendMessage = name => {
+
+function sendMessage(name) {
     const sender = 'Gromcode';
     console.log(`${name}, ${message} Your ${sender}`);
 }
 
-export function setMessage(newText) {
+function setMessage(newText) {
     message = newText;
 }
 
-sendMessage('Ann');
 setMessage('Good job!');
 sendMessage('Ann');
