6c0f4582811d0e7a4bd66b469bf6e8143cfd8f04
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-13 11:28:13 +0300

    js13 commit

--- lesson_13/hw_2/calculator.test.js
+++ lesson_13/hw_2/calculator.test.js
@@ -5,7 +5,22 @@ it('should get not string', () => {
     expect(result).toEqual(null);
 });
 
-it('should get string', () => {
-    const res = calc('+');
-    expect(res).toEqual(a + b);
+it('should get +', () => {
+    const res = calc('1 + 2');
+    expect(res).toEqual('1 + 2 = 3');
+});
+
+it('should get -', () => {
+    const res = calc('3 - 2');
+    expect(res).toEqual('3 - 2 = 1');
+});
+
+it('should get *', () => {
+    const res = calc('2 * 2');
+    expect(res).toEqual('2 * 2 = 4');
+});
+
+it('should get /', () => {
+    const res = calc('6 / 2');
+    expect(res).toEqual('6 / 2 = 3');
 });
