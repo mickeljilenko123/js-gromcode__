ddf67f28c8013f2267f1674a5dd55f867965f1cf
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-14 17:23:13 +0300

    js15 commit

---
+++ lesson_15/task_1/index.js
@@ -0,0 +1,11 @@
+ function makeCounter() {
+     let count = 0;
+     return function count1() {
+         return count++;
+     }
+ }
+ const counter1 = makeCounter();
+ console.log(counter1());
+ console.log(counter1());
+ console.log(counter1());
+ console.log(counter1());
