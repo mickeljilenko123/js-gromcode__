8d660614b2513e09ab23ac8d475a0e1196665f6a
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-14 20:12:49 +0300

    js15 commit

---
+++ lesson_15/task_2/createCalculator.js
@@ -0,0 +1,28 @@
+export function createCalculator() {
+    let num = 0;
+
+    return {
+        add(num1) {
+            return num += num1;
+        },
+
+        decrease(num2) {
+            return num -= num2;
+        },
+        reset() {
+            return num = 0;
+        },
+        getMemo() {
+            return num;
+        },
+    }
+
+}
+const add1 = createCalculator();
+console.log(add1.add(6));
+const decrease1 = createCalculator();
+console.log(decrease1.decrease(2));
+const reset1 = createCalculator();
+console.log(reset1.reset());
+const getMemo1 = createCalculator();
+console.log(getMemo1.getMemo());
