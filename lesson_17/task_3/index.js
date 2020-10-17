a913b7da113bb4dd0021cc01146224ba975c9d1c
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-18 00:54:10 +0300

    js 17 commit

--- lesson_17/task_3/index.js
+++ lesson_17/task_3/index.js
@@ -1,3 +1,20 @@
+//  function defer(func, ms) {
+//      return function() {
+//          setTimeout(() => func(...arguments), ms);
+//      }
+//  };
+//  const obj = {
+//      a: 1,
+//      b: 2,
+//      sum() {
+//          console.log(`${this.a} + ${this.b}`);
+//      }
+//  };
+//  const defered = defer(obj.sum.bind(obj), 1000);
+//  defered();
+
+//=============================================
+
 export function defer(func, ms) {
     return function() {
         setTimeout(() => func.call(this, ...arguments), ms);
@@ -9,4 +26,4 @@ const obj = {
     }
 };
 const defered = defer(obj.sum, 1000);
-defered.call({ a: 5, b: 5 });
+defered.call({ a: 2, b: 2 });
