d0c8a38c7c152a097b8a0622dd9e7028cfd78355
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-19 17:38:56 +0300

    js18 commit

--- lesson_18/task_1/index.js
+++ lesson_18/task_1/index.js
@@ -1,14 +1,14 @@
 const event = {
     guests: [
         { name: 'John', age: 17, email: 'example@server.com' },
-        { name: 'Bob', age: 18, email: 'example@server.com' }
+        { name: 'Bob', age: 18, email: 'example@bob.com' }
     ],
     message: 'Welcome to the party!',
     getInvitations() {
         return this.guests.filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
                 email,
-                text: `Dear ${name}! ${this.message}`,
+                message: `Dear ${name}! ${this.message}`,
             }));
     }
 };
