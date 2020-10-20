c06825687d9ac3bd52dac9575cafd8301d119c46
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-21 00:16:22 +0300

    js18 commit

--- live_coding/this/task_2/index.js
+++ live_coding/this/task_2/index.js
@@ -1,48 +1,48 @@
- function calculator(a, b) {
+export function calculator(a, b) {
 
-     switch (this.operation) {
-         case '+':
-             return a + b;
-         case '-':
-             return a - b;
-         case '/':
-             return a / b;
-         case '*':
-             return a * b;
-         default:
-             return NaN;
-     }
- }
-
-
- /*
-  * Ф-ция multiplier должна быть создана на основе calculator
-  * с использования .bind
-  * и должна принимать 2 числа и возвращать из произведение
-  */
-
- const multiplier = calculator.bind({ operation: '*' }, 4, 5);
- console.log(multiplier());
+    switch (this.operation) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '/':
+            return a / b;
+        case '*':
+            return a * b;
+        case 'n':
+            return a * 2;
+        default:
+            return NaN;
+    }
+}
 
 
- /*
-  * Ф-ция summator должна быть создана на основе calculator
-  * с использования .bind
-  * и должна принимать 2 числа и возвращать из сумму
-  */
- // ...your code here
- const summator = calculator.bind({ operation: '+' }, 4, 5);
- console.log(summator());
- /*
-  * Ф-ция twice должна быть создана на основе calculator
-  * с использования .bind
-  * и должна принимать 1 число и возвращать это число умноженное на 2
-  */
- // ...your code here
+/*
+ * Ф-ция multiplier должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из произведение
+ */
 
- function twice1(n) {
-     return n * 2;
- }
+const multiplier = calculator.bind({ operation: '*' }, 4, 5);
+console.log(multiplier());
 
- const twice = twice1.bind(5);
- console.log(twice());
+
+/*
+ * Ф-ция summator должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 2 числа и возвращать из сумму
+ */
+// ...your code here
+const summator = calculator.bind({ operation: '+' }, 4, 5);
+console.log(summator());
+/*
+ * Ф-ция twice должна быть создана на основе calculator
+ * с использования .bind
+ * и должна принимать 1 число и возвращать это число умноженное на 2
+ */
+// ...your code here
+
+
+const twice = calculator.bind({ operation: 'n' }, 4);
+console.log(twice());
+export { multiplier, summator, twice }
