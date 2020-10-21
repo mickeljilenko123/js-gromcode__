e7a069c7c085eeaa93effdfa870bec354664fcac
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-21 12:18:42 +0300

    js18 commit

--- lesson_17/task_7/index.js
+++ lesson_17/task_7/index.js
@@ -3,14 +3,14 @@ const user = {
     getFullName() {
         return (`${this.firstName} ${this.lastName}`)
     },
-    setFullName(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    setFullName(f, l) {
+        this.firstName = f;
+        this.lastName = l;
     },
 
 };
 
 const res = user.setFullName.bind(user, 'John', 'Doe');
 res();
-// console.log(user.getFullName());
+console.log(user.getFullName());
 export { user }
