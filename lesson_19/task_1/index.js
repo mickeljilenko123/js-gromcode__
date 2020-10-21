ddf26138a17c8b02716b722fa93abf81f1e1d6ad
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-21 22:05:56 +0300

    js19 commit

--- lesson_19/task_1/index.js
+++ lesson_19/task_1/index.js
@@ -12,12 +12,16 @@ const ship = {
     name: 'Argo',
     startMachine() {
         console.log(`${this.name} lifting anchor up`);
+        this.move();
     },
+
+
     stopMachine() {
+        this.stop();
         console.log(`${this.name} lifting anchor down`);
     },
 }
 Object.setPrototypeOf(ship, vehicle);
-ship.move();
-ship.stop();
+ship.startMachine();
+ship.stopMachine();
 export { vehicle, ship }
