7ed63d6786d23c53f2dc13250d0a3b035d3a76dd
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-22 15:11:57 +0300

    js19 commit

--- lesson_19/task_3/index.js
+++ lesson_19/task_3/index.js
@@ -1,21 +1,22 @@
-export function User(name, age) {
-    this.name = name;
-    this.age = age;
-    User.prototype.sayHi = function() {
-        console.log(`Hi, I am ${this.name}`);
-    };
-    User.prototype.requestNewPhoto = function() {
-        console.log(`New photo request was sent for ${this.name}`);
-    };
-    User.prototype.setAge = function() {
-        console.log(`New photo request was sent for ${this.name}`);
-        if (this.age >= 25) {
-            return age;
-        };
-        if (this.age < 0) {
-            return false;
-        };
-    };
-};
-const user1 = new User('John', 29);
-user1.sayHi();
+ function User(name, age) {
+     this.name = name;
+     this.age = age;
+     User.prototype.sayHi = function() {
+         console.log(`Hi, I am ${this.name}`);
+     };
+     User.prototype.requestNewPhoto = function() {
+         console.log(`New photo request was sent for ${this.name}`);
+     };
+     User.prototype.setAge = function(age) {
+         this.age = age;
+         if (age >= 25) {
+             console.log(`New photo request was sent for ${this.name}`);
+             return age;
+         }
+         if (age < 0) {
+             return false;
+         }
+     };
+ };
+ const user1 = new User('John', 29);
+ user1.sayHi();
