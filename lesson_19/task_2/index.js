b892bc8ac1b796730b2474dd2394bee38e5a88e1
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-22 13:15:31 +0300

    js19 commit

--- lesson_19/task_2/index.js
+++ lesson_19/task_2/index.js
@@ -1,10 +1,13 @@
-const obj = {
+const obj1 = {
     name: 'Tom',
     age: 29,
+    sayHi() {
+        console.log('Hi');
+    }
 };
 
 const getOwnProps = (obj) => {
     return Object.keys(obj);
 }
-console.log(getOwnProps(obj));
-export { getOwnProps }
+console.log(getOwnProps(obj1));
+// export { getOwnProps }
