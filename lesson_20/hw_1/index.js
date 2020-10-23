d88824eae914acea07f76b5975b4fe57f0a119e0
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-23 19:50:19 +0300

    js20 commit

---
+++ lesson_20/hw_1/index.js
@@ -0,0 +1,32 @@
+class Order {
+    constructor(price, city, type) {
+        this.id = String(Math.random());
+        this.price = price;
+        this.dateCreated = new Date();
+        this.isConfirmed = false;
+        this.dateConfirmed = new Date();
+        this.city = String(city);
+        this.type = String(type);
+    }
+    checkPrice() {
+        if (this.price > 1000) {
+            return true;
+        }
+        return false;
+    }
+    confirmOrder() {
+        if (this.isConfirmed === false) {
+            this.isConfirmed = true;
+            this.dateConfirmed = new Date();
+            return;
+        }
+    }
+    isValidType() {
+        if (Buy) {
+            return true;
+        }
+        if (Sell) {
+            return false;
+        }
+    }
+}
