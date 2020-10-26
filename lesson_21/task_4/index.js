65a07644a2dea3bc106053e0c8e9c1098816d47a
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-26 21:53:10 +0300

    js21 commit

--- lesson_21/task_4/index.js
+++ lesson_21/task_4/index.js
@@ -1,21 +1,21 @@
 const getTitle = () => {
-    return document.querySelector('.title').textContent;
-}
-getTitle();
+        return document.querySelector('.title').textContent;
+    }
+    // getTitle();
 
 const getDescription = () => {
-    return document.querySelector('.about').innerText;
-}
-getDescription();
+        return document.querySelector('.about').innerText;
+    }
+    // getDescription();
 
 const getPlans = () => {
-    return document.querySelector('.plans').innerHTML;
-}
-getPlans();
+        return document.querySelector('.plans').innerHTML;
+    }
+    // getPlans();
 
 const getGoal = () => {
-    return document.querySelector('.goal').outerHTML;
-}
-getGoal();
+        return document.querySelector('.goal').outerHTML;
+    }
+    // getGoal();
 
 export { getTitle, getDescription, getPlans, getGoal }
