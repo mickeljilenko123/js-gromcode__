cbc38116d4d80ad5213c017a8d6da298a21c5ab3
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-26 20:15:43 +0300

    js21 commit

---
+++ lesson_21/task_2/index.js
@@ -0,0 +1,8 @@
+const getTitleElement = () => {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem)
+}
+const getInputElement = () => {
+    const inputElem = document.querySelector('text');
+    console.dir(inputElem)
+}
