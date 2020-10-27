63752b75553b7cdfbd474c1a2fc5c2f20f69776e
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-27 12:57:57 +0300

    js21 commit

--- lesson_21/task_6/index.js
+++ lesson_21/task_6/index.js
@@ -1,10 +1,10 @@
 const setButton = buttonText => {
     const setButton1 = document.querySelector('body');
-    return setButton1.innerHTML = buttonText;
+    return setButton1.innerHTML = '<button>button text</button>';
 
     // setButton1.innerHTML = buttonText;
     // console.log(setButton1.innerHTML);
 }
-setButton('<button>button text</button>');
+setButton('button text');
 
 export { setButton }
