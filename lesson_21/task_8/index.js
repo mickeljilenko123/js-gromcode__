c176cf5a96023045092db713620cf2a9c100e444
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-27 15:35:13 +0300

    js21 commit

--- lesson_21/task_8/index.js
+++ lesson_21/task_8/index.js
@@ -1,7 +1,9 @@
 const createButton = buttonText => {
     const elem = document.createElement('button');
-    elem.append('body');
     elem.textContent = buttonText;
+    const elem1 = document.querySelector('body');
+    elem1.append(elem);
+
 }
 createButton('Send Email');
 export { createButton }
