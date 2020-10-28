059249d118bce120be64f9b0dbe20bbac68b1cc9
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-28 15:28:16 +0300

    js21 commit

--- lesson_21/task_10/index.js
+++ lesson_21/task_10/index.js
@@ -1,16 +1,16 @@
 const finishForm = () => {
 
-    const getElem = document.querySelector('input');
-    getElem.setAttribute('type', 'text');
-    getElem.setAttribute('name', 'login');
+        const getElem = document.querySelector('input');
+        getElem.setAttribute('type', 'text');
+        getElem.setAttribute('name', 'login');
 
-    const getFinishForm = document.querySelector('.login-form');
-    const elem = document.createElement('input');
-    elem.setAttribute('type', 'password');
-    elem.setAttribute('name', 'password');
+        const getFinishForm = document.querySelector('.login-form');
+        const elem = document.createElement('input');
+        elem.setAttribute('type', 'password');
+        elem.setAttribute('name', 'password');
 
-    getFinishForm.append(elem);
-    console.log(getFinishForm);
-}
-finishForm();
+        getFinishForm.append(elem);
+        // console.log(getFinishForm);
+    }
+    // finishForm();
 export { finishForm }
