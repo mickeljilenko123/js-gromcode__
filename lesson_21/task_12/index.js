61b6e4a3b90b9e885944551f9ace8de8fbae92fd
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-28 23:37:57 +0300

    js21 commit

--- lesson_21/task_12/index.js
+++ lesson_21/task_12/index.js
@@ -1,7 +1,12 @@
 const squaredNumbers = () => {
-    const getElem = document.querySelector('.number');
-    const elemOne = getElem.dataset.number * getElem.dataset.number;
-    getElem.setAttribute('data-squared-number', elemOne);
-    console.log(getElem);
-}
-squaredNumbers();
+        const getElem = document.querySelector('.number');
+        // const elemOne = getElem.dataset.number * getElem.dataset.number;
+        // getElem.setAttribute('data-squared-number', elemOne);
+
+        getElem.forEach(e => {
+                e.dataset.squaredNumber = e.dataset.number * e.dataset.number;
+            })
+            // console.log(getElem);
+    }
+    // squaredNumbers();
+export { squaredNumbers }
