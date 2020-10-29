546384302cde62449f917a8d945cafe6ebdcbb21
mickeljilenko123 <mickeljilenko@gmail.com>
2020-10-29 13:46:15 +0300

    js21 commit

---
+++ lesson_21/task_13/index.js
@@ -0,0 +1,11 @@
+const getSection = num => {
+    const getElem = document.querySelector(`span[data-number="${num}"]`);
+    // console.log(getElem);
+    const getTag = getElem.closest('div');
+    // console.log(getTag);
+    const getDataSection = getTag.getAttribute('data-section');
+    // console.log(getDataSection);
+    return getDataSection;
+}
+getSection(5);
+export { getSection }
