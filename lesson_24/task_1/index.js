d60333658e46792cb2bb61738937852a011f1bcd
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-05 17:06:38 +0300

    js24 commit

---
+++ lesson_24/task_1/index.js
@@ -0,0 +1,10 @@
+// 1) К указаной дате добавить количество дней
+
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+const dayOfWeek = (date, days) => {
+    const day = new Date(date).getDate();
+    const setDays = new Date(date).setDate(day + days);
+    return weekDays[new Date(setDays).getDay()];
+}
+const res = dayOfWeek(new Date(2019, 0, 1), 12);
+console.log(res);
