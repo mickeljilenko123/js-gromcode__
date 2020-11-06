c8805c40d1ea8662845abf9fbc44efdae4cce229
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-06 14:54:28 +0300

    js24 commit

--- lesson_24/hw_1/index.js
+++ lesson_24/hw_1/index.js
@@ -1,3 +1,16 @@
-const elem = document.querySelector('.check');
-elem.innerHTML = '';
-console.log(elem);
+const getDiff = (startDate, endDate) => {
+    let getNewDate = new Date(startDate - endDate);
+
+    if (startDate < endDate) {
+        return getNewDate = new Date(endDate - startDate);
+    }
+
+    let newDay = getNewDate.getDate();
+    let newHour = getNewDate.getHours();
+    let newMin = getNewDate.getMinutes();
+    let newSec = getNewDate.getSeconds();
+    return `${newDay}d ${newHour}h ${newMin}m ${newSec}s`;
+}
+
+const res = getDiff(new Date(2019, 2, 26, 6), 2022, 6, 30, 15);
+console.log(res);
