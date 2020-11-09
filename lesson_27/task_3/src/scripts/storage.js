975f054349b5e4f67eac02ea62d48d1439215f92
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-09 20:56:05 +0300

    js27 commit

--- lesson_27/task_3/src/scripts/storage.js
+++ lesson_27/task_3/src/scripts/storage.js
@@ -1,7 +1,6 @@
 export const setItem = (key, value) => {
     localStorage.setItem(key, JSON.stringify(value));
-    Object.assign(storage, {
-        [key]: value });
+    // Object.assign(storage, { [key]: value });
 };
 
 export const getItem = key => JSON.parse(localStorage.getItem(key));
