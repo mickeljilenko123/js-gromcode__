1afd9a2dfe0a69c488b7273738219779413aa7e4
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-10 14:53:19 +0300

    js29 commit

--- lesson_29/task_1/index.js
+++ lesson_29/task_1/index.js
@@ -21,16 +21,16 @@ const addImage = (imgSrc, callback) => {
 };
 
 //callack example
-const onImageLoaded = (error, elemImage) => {
+const onImageLoaded = (error, data) => {
     if (error) {
         console.log(error);
         return;
     }
-    const { width, height } = elemImage;
+    const { width, height } = data;
     const sizeElem = document.querySelector('.image-size');
     sizeElem.textContent = `${width} x ${height}`;
 }
 
 //test call
-//addImage("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg", onImageLoaded);
+addImage("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg", onImageLoaded);
 export { addImage }
