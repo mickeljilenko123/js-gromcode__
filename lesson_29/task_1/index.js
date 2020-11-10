1d93a92458ac33c7313d39b20e31c26153f0f904
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-10 14:48:19 +0300

    js28 commit

---
+++ lesson_29/task_1/index.js
@@ -0,0 +1,35 @@
+const addImage = (imgSrc, callback) => {
+    // Создаем ел картинки
+    const elemImage = document.createElement('img');
+    elemImage.setAttribute('alt', 'My photo');
+
+    elemImage.src = imgSrc;
+
+    const containerElem = document.querySelector('.page');
+    containerElem.append(elemImage);
+
+    const onImageLoaded = () => {
+        const { width, height } = elemImage;
+        callback(null, { width, height });
+    }
+    elemImage.addEventListener('loaded', onImageLoaded);
+    elemImage.addEventListener('error', () => callback('Image load failed'));
+
+
+    //TODO write your code here
+
+};
+
+//callack example
+const onImageLoaded = (error, elemImage) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    const { width, height } = elemImage;
+    const sizeElem = document.querySelector('.image-size');
+    sizeElem.textContent = `${width} x ${height}`;
+}
+
+//test call
+addImage("https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg", onImageLoaded);
