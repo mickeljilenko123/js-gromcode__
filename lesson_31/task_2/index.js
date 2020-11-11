20459816981c596e9f75af3e259117623aae63f1
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-11 18:30:35 +0300

    js31 commit

---
+++ lesson_31/task_2/index.js
@@ -0,0 +1,21 @@
+const asyncCalculator = numbr =>
+    new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Initial value: ${numbr}`)
+            resolve(numbr);
+        }, 5000)
+    })
+    .then(value => new Promise((resolve) => {
+        setTimeout(() => {
+            const result = value * value;
+            console.log(`Squared value: ${result}`);
+            resolve(result)
+        }, 5000)
+    }))
+    .then(value => {
+        const result = value * 2;
+        console.log(`Doubled value: ${result}`);
+        return result;
+    })
+asyncCalculator(5)
+    .then(value => console.log(value))
