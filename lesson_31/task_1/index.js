8b872c9ea2e4111287ee9a8e2c9ece347b4542f8
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-11 16:19:39 +0300

    js31 commit

---
+++ lesson_31/task_1/index.js
@@ -0,0 +1,23 @@
+const requestUserData = (userId) => {
+    const p = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'))
+            }, 500);
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    age: 17,
+                    userId,
+                    email: `${userId}`,
+                })
+            }, 1000);
+        };
+    });
+
+    return p;
+};
+requestUserData('userid777')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
