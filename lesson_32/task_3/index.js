7c3a036fec66fb974b49cf1143b9240b1b77d868
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-12 16:41:27 +0300

    js32 commit

--- lesson_32/task_3/index.js
+++ lesson_32/task_3/index.js
@@ -2,7 +2,7 @@
  * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
  */
 
-const makePromise = num =>
+export const makePromise = num =>
     Promise.resolve(num);
 
 // const makePromise = Promise.resolve(num);
