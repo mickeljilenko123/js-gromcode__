5f2de8f528e7a7d7767e84597247f595831bc846
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-17 17:07:18 +0300

    js27 commit

--- lesson_27/task_2/index.js
+++ lesson_27/task_2/index.js
@@ -1,39 +1,31 @@
 const counterElem = document.querySelector('.counter');
-const counterValueElem = document.querySelector('.counter__value');
+const spanElem = document.querySelector('.counter__value');
 
-const onCounterChange = e => {
-    console.log(e)
+const onClickElem = e => {
+    //Проверяем был ли клик на кнопке
     const isButton = e.target.classList.contains('counter__button');
-
     if (!isButton) {
         return;
     }
-
     const action = e.target.dataset.action;
-
-    const oldValue = Number(counterValueElem.textContent);
+    const oldValue = Number(spanElem.textContent);
 
     const newValue = action === 'decrease' ?
-        oldValue - 1 :
-        oldValue + 1;
+        oldValue - 1 : oldValue + 1;
 
     localStorage.setItem('counterValue', newValue);
 
-    counterValueElem.textContent = newValue;
-};
-
-counterElem.addEventListener('click', onCounterChange);
-
+    spanElem.textContent = newValue;
+}
+counterElem.addEventListener('click', onClickElem);
 
 const onStorageChange = e => {
-    console.log(e)
-    counterValueElem.textContent = e.newValue;
-};
+    spanElem.textContent = e.newValue;
+}
 
 window.addEventListener('storage', onStorageChange);
 
 const onDocumentLoaded = () => {
-    counterValueElem.textContent = localStorage.getItem('counterValue') || 0;
-};
-
-document.addEventListener('DOMcContentLoaded', onDocumentLoaded);
+    spanElem.textContent = localStorage.getItem('counterValue') || 0;
+}
+document.addEventListener('DOMContentLoaded', onDocumentLoaded)
