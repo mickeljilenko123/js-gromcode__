62388dccea5770f8828d4b82148e7e873d2beb85
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-17 16:02:14 +0300

    js27 commit

--- lesson_27/task_1/index.js
+++ lesson_27/task_1/index.js
@@ -1,6 +1,6 @@
 localStorage.clear();
 localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
-localStorage.setItem('name', 'Tom');
+localStorage.setItem('name', JSON.stringify('Tom'));
 localStorage.setItem('age', JSON.stringify(17));
 
 const getLocalStorageData = () => {
@@ -14,8 +14,8 @@ const getLocalStorageData = () => {
             }
             return {
                 ...acc,
-                [key]: newValue,
-            };
+                [key]: JSON.parse(value),
+            }
         }, {});
 };
 
