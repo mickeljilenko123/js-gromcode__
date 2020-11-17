155e4d2f8495d28477aaa5e06170afa0aa5c6e88
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-18 00:08:58 +0300

    js27 commit

--- lesson_27/task_3/index.js
+++
@@ -1,7 +0,0 @@
-import { renderListItems } from './render.js';
-import { tasks } from './tasks.js';
-import { addTask } from './createTask.js';
-import { elementStatus } from './updateTask.js';
-
-
-renderListItems(tasks);
--- lesson_27/task_3/src/index.js
+++
@@ -1,8 +0,0 @@
-import { renderListItems } from './render.js';
-import { tasks } from './tasks';
-import { addTask } from './createTask';
-import { elementStatus } from './updateTask';
-
-const listElement = document.querySelector(".list");
-const inputEl = document.querySelector(".task-input");
-const createButton = document.querySelector(".create-task-btn");
