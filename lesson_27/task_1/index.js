efafe5b0789abaf8804befd77e1f1b1b9854dd90
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-17 17:12:17 +0300

    js27 commit

--- lesson_27/task_1/index.js
+++ lesson_27/task_1/index.js
@@ -14,7 +14,7 @@ const getLocalStorageData = () => {
             }
             return {
                 ...acc,
-                [key]: value,
+                [key]: newValue,
             }
         }, {});
 };
