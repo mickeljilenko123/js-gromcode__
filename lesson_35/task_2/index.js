6f8d930d52a2278eb9f454d4a673973a2588d2cd
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-19 23:19:03 +0300

    js35 commit

--- lesson_35/task_2/index.js
+++ lesson_35/task_2/index.js
@@ -2,7 +2,7 @@ const successRequest = Promise.resolve({ name: "Tom" });
 
 successRequest
     .then(function onSuccess1(data) {
-        console.log('Error with data', data);
+        throw new Error('Error with data');
     })
     .catch(function onError1(error) {
         console.error("onError1", error.message);
@@ -14,7 +14,7 @@ const failRequest = Promise.reject(new Error("Something went wrong"));
 failRequest
     .catch(function onError2(error) {
         console.error("onError2", error.message);
-        console.log('Server error', error.message);
+        throw new Error('Server error');
     })
     .then(function onSuccess2(data) {
         console.log("onSuccess2", data);
