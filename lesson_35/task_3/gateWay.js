72c7527c64c70aa2b2050665f945016b661ca667
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-20 19:47:14 +0300

    js35 commit

---
+++ lesson_35/task_3/gateWay.js
@@ -0,0 +1,19 @@
+export const fetchUserData = (userName) => {
+    return fetch(`https://api.github.com/users/${userName}`)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Failed to load error')
+        });
+
+};
+export const fetchRepositories = url => {
+    return fetch(url)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Failed to load error')
+        });
+};
---
+++ lesson_35/task_3/index.js
@@ -0,0 +1,47 @@
+import { fetchUserData, fetchRepositories } from './gateWay.js';
+import { renderUserData } from './user.js';
+import { renderRepos, cleanReposList } from './repos.js';
+import { showSpinner, hideSpinner } from './spinner.js';
+
+
+const defaultUser = {
+    avatar_url: 'https:avatars3.githubusercontent.com/u10001',
+    name: '',
+    location: '',
+};
+renderUserData(defaultUser);
+
+
+const showUserBtnElem = document.querySelector(".name-form__btn");
+const userNameInputElem = document.querySelector(".name-form__input");
+
+
+const onSearchUser = () => {
+    showSpinner();
+    cleanReposList();
+    const userName = userNameInputElem.value;
+    console.log(userName);
+    //Запрашиваем данные в пользователя
+    fetchUserData(userName)
+        .then((userData) => {
+            // Полученные данные отрисовали
+            renderUserData(userData)
+                // Передаем управелние дальше
+            return userData.repos_url
+        })
+        // Делаем запрос за списком
+        .then(url => fetchRepositories(url))
+        // И после этого его отрисовываем
+        .then(reposList => {
+            renderRepos(reposList);
+        })
+        .catch(err => {
+            alert(err.message)
+        })
+        .finally(() => {
+            hideSpinner();
+        })
+
+};
+
+showUserBtnElem.addEventListener("click", onSearchUser);
