87c754281c3b79c64a09fba03a21a15d67795dde
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-20 19:58:13 +0300

    js35 commit

--- lesson_35/task_3/spinner.js
+++ lesson_35/task_3/spinner.js
@@ -1,9 +1,9 @@
 const spinnerElem = document.querySelector('.spinner');
 
 export const showSpinner = () => {
-    spinnerElem.classList.remove('.spinner_hidden')
+    spinnerElem.classList.remove('spinner_hidden')
 }
 
 export const hideSpinner = () => {
-    spinnerElem.classList.add('.spinner_hidden')
+    spinnerElem.classList.add('spinner_hidden')
 }
