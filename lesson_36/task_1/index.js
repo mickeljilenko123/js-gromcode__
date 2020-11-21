8614b58c3898c13fb132dbf0e8d84e7c191c0e1d
mickeljilenko123 <mickeljilenko@gmail.com>
2020-11-21 14:54:33 +0300

    js36 commit

---
+++ lesson_36/task_1/index.js
@@ -0,0 +1,15 @@
+const fetchUser = async userId => {
+    try {
+        const response = await fetch(`https://api.github.com/users/${userId}`);
+        if (!response.ok) {
+            return null;
+        }
+        const userData = await response.json();
+        return userData;
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+fetchUser('facebook')
+    .then(userData => console.log(userData));
